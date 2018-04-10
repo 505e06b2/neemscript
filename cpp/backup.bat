05251c98f5dbc7319e510d29632bbdd53fbd2e14
505e06b2 <505e06b2@gmail.com>
2018-04-10 18:30:46 +0100

    Sorted out backup script and made sure everything is up-to-date before checking out

--- cpp/backup.bat
+++ cpp/backup.bat
@@ -3,6 +3,7 @@ set /p COMMIT=Commit:
 if "%COMMIT%"=="" goto :eof
 ::Need this line since make seems to exit the shell and we also want the variables sandboxes
 cmd /c buildjs.bat
+make -j5
 
 cd ..
 git add *
