dc114c8f3ad7e5746662d6a69bb8cb4cbb93b537
505e06b2 <505e06b2@gmail.com>
2018-04-12 05:48:52 +0100

    Error if chdir fails

--- cpp/src/neem.cpp
+++ cpp/src/neem.cpp
@@ -300,7 +300,10 @@ bool Neem::parseline(char *line) {
 				else last->value = params; //Directory name
 			last->func = [this](instruction *i, uint32_t index) {
 				if(i->extravalue != "") fprintf(outputhandle, "%s\n", getcurrentdir().c_str()); //Print if no args
-					else chdir(parsevarval(&i->value).c_str());
+					else if(chdir(parsevarval(&i->value).c_str()) != 0) {
+						std::string parsed = parsevarval(&i->value);
+						return alert('!', "Can't cd to '%s'", &index, &parsed);
+					}
 				return -1;
 			};
 			break;
