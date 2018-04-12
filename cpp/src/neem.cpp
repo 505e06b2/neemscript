8452176edfb12b7d08d03848d49bbccfd3ead105
505e06b2 <505e06b2@gmail.com>
2018-04-12 04:53:11 +0100

    Allow for emacs-style header

--- cpp/src/neem.cpp
+++ cpp/src/neem.cpp
@@ -32,6 +32,7 @@ Neem::types Neem::gettype(char *command) {
 	if(strcasecmp(command, "loadlib") == 0) return loadlib_;
 	if(strcasecmp(command, "unloadlib") == 0) return unloadlib_;
 	if(strcasecmp(command, "runlibfunc") == 0) return runlibfunc_;
+	if(command[0] == '-') return comment_;
 	if(command[0] == '#') return comment_;
 	if(command[0] == ':' && command[1] != ':') return label_;
 	if(command[0] == ':' && command[1] == ':') return comment_;
