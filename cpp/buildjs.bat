f3fa610dcff73c2d0313b83e08da3623d6e081c1
505e06b2 <505e06b2@gmail.com>
2018-04-14 12:53:55 +0100

    Fixed the html file, split the css, added an asmjs fallback, altered makefile to generate both wasm and asmjs for browser

--- cpp/buildjs.bat
+++ cpp/buildjs.bat
@@ -1,4 +1,4 @@
 @echo off
 if "%ALREADY%"=="" call C:\Users\x\Home\emsdk-master\emsdk_env.bat
 set ALREADY=finished
-emmake make -j5 web OPTIMISATIONS=-O3 LDFLAGS=
+emmake make -j5 web OPTIMISATIONS=-O3 LDFLAGS="-s ASSERTIONS=1 -s EXPORTED_FUNCTIONS='[""_webmain""]' -s EXTRA_EXPORTED_RUNTIME_METHODS='[""cwrap""]'"
